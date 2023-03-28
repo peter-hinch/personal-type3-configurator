c5a50f37727d0ba1e03ef182eea364bee666ade8
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-03-28 23:18:30 +1100

    Add code to allow switching of bodyStyle.

--- src/Car.tsx
+++ src/Car.tsx
@@ -1,7 +1,10 @@
 import React, { useLayoutEffect, useEffect } from 'react';
 import { useGLTF } from '@react-three/drei';
 
-const Car: React.FC<{ props: any }> = ({ props }) => {
+const Car: React.FC<{ settings: Settings; props: any }> = ({
+  settings,
+  props
+}) => {
   const { scene, nodes, materials } = useGLTF('/volkswagen-type3.glb');
 
   useLayoutEffect(() => {
@@ -9,11 +12,35 @@ const Car: React.FC<{ props: any }> = ({ props }) => {
       (obj) =>
         obj.type === 'Mesh' && (obj.receiveShadow = obj.castShadow = true)
     );
-    if (nodes.rear_windows_fastback && nodes.roof_skin_fastback) {
-      nodes.rear_windows_fastback.visible = false;
-      nodes.roof_skin_fastback.visible = false;
-    }
+    // console.log(scene);
+    // console.log(nodes);
+    // console.log(materials);
   }, [scene, nodes, materials]);
+
+  useEffect(() => {
+    if (
+      nodes.rear_windows_square &&
+      nodes.roof_skin_square &&
+      nodes.boot_square &&
+      nodes.rear_windows_fastback &&
+      nodes.roof_skin_fastback
+    ) {
+      if (settings.bodyStyle === 'square') {
+        nodes.roof_skin_square.visible = true;
+        nodes.rear_windows_square.visible = true;
+        nodes.boot_square.visible = true;
+        nodes.rear_windows_fastback.visible = false;
+        nodes.roof_skin_fastback.visible = false;
+      }
+      if (settings.bodyStyle === 'fastback') {
+        nodes.roof_skin_square.visible = false;
+        nodes.rear_windows_square.visible = false;
+        nodes.boot_square.visible = false;
+        nodes.rear_windows_fastback.visible = true;
+        nodes.roof_skin_fastback.visible = true;
+      }
+    }
+  }, [nodes, settings]);
 
   return <primitive object={scene} {...props} />;
 };
