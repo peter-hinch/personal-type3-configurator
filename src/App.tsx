9fa96353a3ecdad26593fed01adc14b16991de8c
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2023-03-28 22:34:05 +1100

    Add settings pane.

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,63 @@
 // Reference: https://codesandbox.io/s/q48jgy
 // Reference: https://codesandbox.io/s/9b56t
 
-import React, { Suspense } from 'react';
+import React, { useState, Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
 import {
   PresentationControls,
   Stage,
   MeshReflectorMaterial
 } from '@react-three/drei';
+
 // @ts-ignore
 import Car from './Car.tsx';
+// @ts-ignore
+import Controls from './Controls.tsx';
 
 const App: React.FC = () => {
+  const [settings, setSettings] = useState({ bodyStyle: 'square' });
+
+  const handleSettings = (value: string) => {
+    console.log(value);
+    setSettings((prevState) => ({ ...prevState, bodyStyle: value }));
+  };
+
   return (
-    <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
-      <color attach="background" args={['#101010']} />
-      <fog attach="fog" args={['#101010', 10, 20]} />
-      <Suspense fallback={null}>
-        <PresentationControls
-          speed={1.5}
-          global
-          zoom={0.7}
-          polar={[-0.1, Math.PI / 4]}
-        >
-          <Stage>
-            <Car position={[0, 0, 0]} scale={1} rotation-y={-Math.PI / 4} />
-          </Stage>
-          <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.72, 0]}>
-            <planeGeometry args={[170, 170]} />
-            <MeshReflectorMaterial
-              blur={[300, 100]}
-              mirror={0.5}
-              resolution={2048}
-              mixBlur={1}
-              mixStrength={40}
-              roughness={1}
-              depthScale={1.2}
-              minDepthThreshold={0.4}
-              maxDepthThreshold={1.4}
-              color="#101010"
-              metalness={0.5}
-            />
-          </mesh>
-        </PresentationControls>
-      </Suspense>
-    </Canvas>
+    <>
+      <Controls settings={settings} handleSettings={handleSettings} />
+      <Canvas dpr={[1, 2]} shadows camera={{ fov: 45 }}>
+        <color attach="background" args={['#101010']} />
+        <fog attach="fog" args={['#101010', 10, 20]} />
+        <Suspense fallback={null}>
+          <PresentationControls
+            speed={1.5}
+            global
+            zoom={0.7}
+            polar={[-0.1, Math.PI / 4]}
+          >
+            <Stage>
+              <Car position={[0, 0, 0]} scale={1} rotation-y={-Math.PI / 4} />
+            </Stage>
+            <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.72, 0]}>
+              <planeGeometry args={[170, 170]} />
+              <MeshReflectorMaterial
+                blur={[300, 100]}
+                mirror={0.5}
+                resolution={2048}
+                mixBlur={1}
+                mixStrength={40}
+                roughness={1}
+                depthScale={1.2}
+                minDepthThreshold={0.4}
+                maxDepthThreshold={1.4}
+                color="#101010"
+                metalness={0.5}
+              />
+            </mesh>
+          </PresentationControls>
+        </Suspense>
+      </Canvas>
+    </>
   );
 };
 
---
+++ src/Controls.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import styled from 'styled-components';
+
+type Settings = { bodyStyle: string };
+
+const Controls: React.FC<{ settings: Settings; handleSettings: Function }> = ({
+  settings,
+  handleSettings
+}) => {
+  return (
+    <StyledControls>
+      <fieldset>
+        <input
+          type="radio"
+          name="body-style"
+          id="square"
+          checked={settings.bodyStyle === 'square'}
+          onClick={(e) => handleSettings(e.target.id)}
+        />
+        <label htmlFor="square">Squareback</label>
+        <input
+          type="radio"
+          name="body-style"
+          id="fastback"
+          checked={settings.bodyStyle === 'fastback'}
+          onClick={(e) => handleSettings(e.target.id)}
+        />
+        <label htmlFor="fastback">Fastback</label>
+      </fieldset>
+    </StyledControls>
+  );
+};
+
+const StyledControls = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 100;
+
+  fieldset {
+    border: none;
+    background: white;
+  }
+`;
+
+export default Controls;
