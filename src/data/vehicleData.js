a4edfc56c38075a583b67461ab7f7d6619d0c12d
peter-hinch <62582081+peter-hinch@users.noreply.github.com>
2024-10-03 15:57:25 +1000

    Addd colours.

--- src/data/vehicleData.js
+++ src/data/vehicleData.js
@@ -16,6 +16,30 @@ export const vehicleData = {
       paintCode: 'L41'
     },
     {
+      id: 'ruby_red',
+      name: 'Ruby Red',
+      hex: '#731b1a',
+      yearMin: 1961,
+      yearMax: 1965,
+      paintCode: 'L456'
+    },
+    {
+      id: 'gulf_blue',
+      name: 'Gulf Blue',
+      hex: '#354c5e',
+      yearMin: 1961,
+      yearMax: 1963,
+      paintCode: 'L390'
+    },
+    {
+      id: 'sea_blue',
+      name: 'Sea Blue',
+      hex: '#274c5f',
+      yearMin: 1963,
+      yearMax: 1965,
+      paintCode: 'L360'
+    },
+    {
       id: 'pearl_white',
       name: 'Pearl White',
       hex: '#ddd9ce',
@@ -24,12 +48,36 @@ export const vehicleData = {
       paintCode: 'L87'
     },
     {
-      id: 'baltic_blue',
-      name: 'Baltic Blue',
-      hex: '#697c8b',
-      yearMin: 1965,
-      yearMax: 1967,
-      paintCode: 'L539'
+      id: 'anthracite',
+      name: 'Anthracite',
+      hex: '#3c3b39',
+      yearMin: 1961,
+      yearMax: 1963,
+      paintCode: 'L469'
+    },
+    {
+      id: 'nutria',
+      name: 'Nutria',
+      hex: '#4e433d',
+      yearMin: 1963,
+      yearMax: 1964,
+      paintCode: 'L575'
+    },
+    {
+      id: 'birch_green',
+      name: 'Birch Green',
+      hex: '#88a682',
+      yearMin: 1961,
+      yearMax: 1964,
+      paintCode: 'L511'
+    },
+    {
+      id: 'safari_beige',
+      name: 'Safari Beige',
+      hex: '#817b57',
+      yearMin: 1961,
+      yearMax: 1965,
+      paintCode: 'L563'
     },
     {
       id: 'sea_sand',
@@ -40,20 +88,28 @@ export const vehicleData = {
       paintCode: 'L568'
     },
     {
+      id: 'velvet_green',
+      name: 'Velvet Green',
+      hex: '#31493b',
+      yearMin: 1964,
+      yearMax: 1965,
+      paintCode: 'L512'
+    },
+    {
+      id: 'baltic_blue',
+      name: 'Baltic Blue',
+      hex: '#697c8b',
+      yearMin: 1965,
+      yearMax: 1967,
+      paintCode: 'L539'
+    },
+    {
       id: 'vw_blue',
       name: 'VW Blue',
       hex: '#1d2343',
       yearMin: 1966,
       yearMax: 1968,
       paintCode: 'L633'
-    },
-    {
-      id: 'delta_green',
-      name: 'Delta Green',
-      hex: '#202f2a',
-      yearMin: 1966,
-      yearMax: 1967,
-      paintCode: 'L610'
     },
     {
       id: 'granada_red',
@@ -64,12 +120,76 @@ export const vehicleData = {
       paintCode: 'L650'
     },
     {
+      id: 'delta_green',
+      name: 'Delta Green',
+      hex: '#202f2a',
+      yearMin: 1966,
+      yearMax: 1967,
+      paintCode: 'L610'
+    },
+    {
+      id: 'peru_green',
+      name: 'Peru Green',
+      hex: '#1a3d37',
+      yearMin: 1967,
+      yearMax: 1969,
+      paintCode: 'L60 B'
+    },
+    {
+      id: 'deep_sea_green',
+      name: 'Deep Sea Green',
+      hex: '#060f0a',
+      yearMin: 1967,
+      yearMax: 1970,
+      paintCode: 'L66 B'
+    },
+    {
       id: 'lotus_white',
       name: 'Lotus White',
       hex: '#e4dfc2',
       yearMin: 1966,
       yearMax: 1968,
       paintCode: 'L282'
+    },
+    {
+      id: 'savannah_beige',
+      name: 'Savannah Beige',
+      hex: '#92866c',
+      yearMin: 1967,
+      yearMax: 1970,
+      paintCode: 'L620'
+    },
+    {
+      id: 'royal_red',
+      name: 'Royal Red',
+      hex: '#691411',
+      yearMin: 1968,
+      yearMax: 1970,
+      paintCode: 'L30 A'
+    },
+    {
+      id: 'diamond_blue',
+      name: 'Diamond Blue',
+      hex: '#a4b6b6',
+      yearMin: 1968,
+      yearMax: 1970,
+      paintCode: 'L50 B'
+    },
+    {
+      id: 'chinchilla',
+      name: 'Chinchilla',
+      hex: '#949285',
+      yearMin: 1968,
+      yearMax: 1970,
+      paintCode: 'L70 F'
+    },
+    {
+      id: 'toga_white',
+      name: 'Toga White',
+      hex: '#bec1ae',
+      yearMin: 1968,
+      yearMax: 1969,
+      paintCode: 'L90 C'
     }
   ],
   wheels: [{ id: '2litre', name: '2 Litre Alloy' }]
